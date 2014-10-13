0eee19b49fec6493b9a5ea8d8acadd52a4fbedd6
VirginiaPereiro <vir_pereiro@hotmail.com>
2014-10-13 09:58:38 +0200

    Rejoj Web

---
+++ WebWorker/js/Lanzador.js
@@ -0,0 +1,18 @@
+﻿var trabajador;
+function iniciar() {
+    if (typeof (Worker) !== "undefined") {
+        trabajador = new Worker("js/workerReloj.js");//establecer la ruta relativa en el caso de que esté guardado en otro repositorio
+    }
+    trabajador.onmessage = function(evt) {
+        document.getElementById("reloj").innerHTML = evt.data;
+    };
+    trabajador.onerror = function(err) {
+        alert(err.data);
+    };
+}
+
+(function () {
+
+    iniciar();
+
+})();
---
+++ WebWorker/js/workerReloj.js
@@ -0,0 +1,10 @@
+﻿function calcularHora() {
+    var hora = new Date();
+    var horaTexto = hora.getHours()+ ":" + hora.getMinutes() + ":" + hora.getSeconds();
+
+    postMessage(horaTexto);
+    setTimeout("calcularHora()", 1000);//ejecuta la función de una forma defirida
+                                    //esperas un segundo y vuelves a ejecutarlo.
+}
+
+calcularHora();
